356c697bc6e6b084ab77ef9a0bef17f8a547dcf9
Dutchramen <64718226+Dutchramen@users.noreply.github.com>
2021-04-09 11:07:00 -0700

    Update ReservationService.java

--- Hotel Reservation/src/service/ReservationService.java
+++ Hotel Reservation/src/service/ReservationService.java
@@ -12,6 +12,8 @@ public class ReservationService {
     public Collection<Reservation> reservations = new HashSet<>();
     public Collection<IRoom> rooms = new HashSet<>();
 
+    //private constructor to facilitate the Singleton Pattern
+    // for the "There can be only One!!!!" instance of this class
     private ReservationService() {
     }
 
@@ -23,33 +25,43 @@ public class ReservationService {
         return reservationService;
     }
 
+    //method to add rooms to Collection<IRoom> rooms
     public void addRoom(IRoom room){
         rooms.add(room);
     }
 
+    //method that should return roomId if roomId is available and
     public IRoom getARoom(String roomId) {
         for (IRoom room : rooms) {
-            System.out.println(roomId);
+            if (rooms.contains(roomId)){
+                System.out.println(roomId);
+            }
+            return room;
         }
-        return rooms;
+        return null;
     }
 
     public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
-        Reservation reservedRoom = new Reservation(customer,room, checkInDate, checkOutDate);
-        reservations.add(reservedRoom);
-        return reservedRoom;
+        Reservation reservedRooms = new Reservation(customer,room, checkInDate, checkOutDate);
+        reservations.add(reservedRooms);
+        return reservedRooms;
     }
 
-    public Collection<IRoom> findARoom(Date checkInDate, Date checkOutDate){
-        Iterator<IRoom> iRoomIterator = rooms.iterator();
-        while (iRoomIterator.hasNext()){
-            System.out.println(iRoomIterator.next());
+    public Collection<IRoom> findARoom(Date checkInDate, Date checkOutDate) {
+        for (IRoom room : rooms) {
+            if (checkInDate.after(checkInDate) && checkOutDate.before(checkOutDate)) {
+                Iterator<IRoom> iRoomIterator = rooms.iterator();
+                while (iRoomIterator.hasNext()) {
+                    System.out.println(iRoomIterator.next());
+                }
+                return rooms;
+            }
         }
-        return rooms;
+        return null;
     }
 
     public Collection<Reservation> getCustomerReservation(Customer customer){
-
+        return reservations;
     }
 
     public void printAllReseverations(){
