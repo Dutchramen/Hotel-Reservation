2e77767ab965c6b406cf1f63b1b6f9f29c8d8099
Dutchramen <64718226+Dutchramen@users.noreply.github.com>
2021-04-09 14:34:29 -0700

    Update ReservationService.java

--- Hotel Reservation/src/service/ReservationService.java
+++ Hotel Reservation/src/service/ReservationService.java
@@ -3,9 +3,11 @@ package service;
 import model.Customer;
 import model.IRoom;
 import model.Reservation;
-import model.Room;
 
-import java.util.*;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class ReservationService {
     private static ReservationService reservationService;
@@ -31,18 +33,18 @@ public class ReservationService {
     }
 
     //method that should return roomId if roomId is available and
-    public IRoom getARoom(String roomId) {
+    public IRoom getARoom(String roomNumber) {
         for (IRoom room : rooms) {
-            if (rooms.contains(roomId)){
-                System.out.println(roomId);
+            if (roomNumber.equals(room.getRoomNumber())) {
+                return room;
             }
-            return room;
         }
         return null;
     }
 
-    public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate){
-        Reservation reservedRooms = new Reservation(customer,room, checkInDate, checkOutDate);
+
+    public Reservation reserveARoom(String customer, IRoom room, Date checkInDate, Date checkOutDate){
+        Reservation reservedRooms = new Reservation(customer,room,checkInDate, checkOutDate);
         reservations.add(reservedRooms);
         return reservedRooms;
     }
@@ -61,6 +63,7 @@ public class ReservationService {
     }
 
     public Collection<Reservation> getCustomerReservation(Customer customer){
+        CustomerService.getInstance().getCustomer(customer.getEmail());
         return reservations;
     }
 
