11a1f4334a6b3e6a2746d5b550da0f1bf1f25395
Dutchramen <64718226+Dutchramen@users.noreply.github.com>
2021-04-10 16:20:53 -0700

    Add files via upload

---
+++ Hotel Reservation/src/api/HotelResource.java
@@ -0,0 +1,50 @@
+package api;
+
+import model.Customer;
+import model.IRoom;
+import model.Reservation;
+import service.CustomerService;
+import service.ReservationService;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class HotelResource {
+    private static HotelResource hotelResource;
+    public static CustomerService customerService = CustomerService.getInstance();
+    public static ReservationService reservationService = ReservationService.getInstance();
+
+    //static reference
+    private HotelResource(){
+    }
+
+    public Customer getCustomer(String email){
+        return customerService.getCustomer(email);
+    }
+
+    public void createACustomer(String email, String firstName, String lastName){
+        customerService.addCustomer(email, firstName,lastName);
+    }
+
+    public IRoom getRoom(String roomNumber){
+        return reservationService.getARoom(roomNumber);
+    }
+
+    public Reservation bookARoom(String customerEmail, IRoom room, Date checkInDate, Date checkOutDate){
+        return reservationService.reserveARoom(customerEmail,room,checkInDate,checkOutDate);
+    }
+
+    public Collection<Reservation> getCustomerReservations(String customerEmail){
+        return reservationService.getCustomerReservation(customerEmail);
+    }
+
+    public Collection<IRoom> findARoom(Date checkInDate, Date checkOutDate){
+        return reservationService.findARoom(checkInDate, checkOutDate);
+    }
+
+
+
+
+
+
+}
