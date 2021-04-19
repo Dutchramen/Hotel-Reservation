aa401fa173fad9a043be2d48c1a3a5a60af74f13
Dutchramen <64718226+Dutchramen@users.noreply.github.com>
2021-04-18 17:47:25 -0700

    Add files via upload

--- Hotel Reservation/src/model/Driver.java
+++ Hotel Reservation/src/model/Driver.java
@@ -1,19 +1,19 @@
 package model;
 
 import api.AdminMenu;
-import api.HotelResource;
 import api.MainMenu;
 
-import java.util.Date;
+
 
 public class Driver {
     public static void main(String[] args) {
 
+
+
+
+
         MainMenu.selectMainMenuOptions();
 
-//        MainMenu.selectMainMenuOptions();
-//        AdminMenu.selectMainMenuOptions();
-//        AdminMenu.seeAllCustomers();
 
 
 
